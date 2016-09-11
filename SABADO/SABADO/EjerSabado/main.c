f0b2d15361595a06e9376a19ae64037e0285e99f
sabsolvei <sabrinaveiga@hotmail.com>
2016-09-11 12:37:53 -0300

    Ejer Sabado

---
+++ SABADO/SABADO/EjerSabado/main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+#define E 4
+
+
+int main()
+{
+    int opcion;
+    long int dni[E];
+    char nombreApellido[E][30];
+    char domicilio[E][25];
+    char localidad[E][20];
+    int edad[E];
+    char categoria[E];
+    int hsTrabajadas [E];
+    float sueldoNeto[E];
+    int i=0;
+    int j;
+    int cont=0;
+
+    do
+    {
+        printf("\n1. Altas.\n2. Modificacion.\n3. Listar.\n4. Buscar por localidad.\n5. Buscar empleados con maximo sueldo.\n6. Buscar empleados con minimo sueldo.\n7. Salir.\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+
+            system("cls");
+
+            printf("Ingrese numero de documento: ");
+            scanf("%ld",&dni[cont]);
+            system("cls");
+
+            fflush(stdin);
+            printf("Ingrese Nombre/s y Apellido/s: ");
+            gets(nombreApellido[cont]);
+            system("cls");
+
+            fflush(stdin);
+            printf("Ingrese domicilio: ");
+            gets(domicilio[cont]);
+            system("cls");
+
+            fflush(stdin);
+            printf("Ingrese localidad: ");
+            gets(localidad[cont]);
+            system("cls");
+
+            fflush(stdin);
+            printf("Ingrese edad: ");
+            scanf("%d",&edad[cont]);
+            system("cls");
+
+            printf("Ingrese la categoria (A, B o C): ");
+            categoria[cont]=toupper(getche());
+            system("cls");
+
+            fflush(stdin);
+            printf("Ingrese la  cantidad de horas trabajadas: ");
+            scanf("%d",&hsTrabajadas[cont]);
+            system("cls");
+
+            switch(categoria[cont])
+            {
+                case 'A':
+                    sueldoNeto[cont]= hsTrabajadas[cont]*50;
+                break;
+
+                case 'B':
+                    sueldoNeto[cont]= hsTrabajadas[cont]*30.5;
+                break;
+
+                case 'C':
+                    sueldoNeto[cont]= hsTrabajadas[cont]*20.67;
+                break;
+            }
+            system("cls");
+            cont++;
+
+
+            case 3:
+                for(i=0;i<E;i++)
+                {
+                    printf("%ld\t%s\t%d\t%s\t%s\t%c\t%d\t%d",dni[i],nombreApellido[i],edad[i],localidad[i],domicilio[i],categoria[i],hsTrabajadas[i],sueldoNeto[i]);
+                }
+
+            break;
+
+        }
+    }
+    while(opcion!=7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
